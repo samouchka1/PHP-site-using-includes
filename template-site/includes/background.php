fef1e9de75bc5ca119a0e3f6a4bcf830bc2bdbfd
samouchka1 <82068124+samouchka1@users.noreply.github.com>
2022-04-24 16:19:08 -0400

    Delete template-site directory

--- template-site/includes/background.php
+++
@@ -1,8 +0,0 @@
-<style>
-
-    body {
-        background: linear-gradient(180deg, rgba(216,221,251,1) 0%, rgba(234,246,251,1) 50%, rgba(216,221,251,1) 100%);
-        background-size: cover;
-    }
-    
-</style>
