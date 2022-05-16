62c04dd2c88b9dfec9273a1d270842d2e2398bbd
samouchka1 <82068124+samouchka1@users.noreply.github.com>
2022-05-15 23:29:09 -0400

    Add files via upload

---
+++ includes/title.php
@@ -0,0 +1,32 @@
+<style>
+:root {
+    --menus-blue: rgba(14, 32, 133, 0.7);
+    --navbar-bg-blue: linear-gradient(55deg, rgba(31,44,134,1) 0%, rgba(75,134,173,1) 100%);
+}
+
+.title-area {
+    background: var(--navbar-bg-blue);
+    /* background: url(); */
+}
+
+.title {
+    color: rgba(255, 255, 255, 0.75);
+    text-align: center;
+    padding: 4rem;
+    font-family: 'Josefin Sans', sans-serif;
+    transition: .2s;
+}
+    .title:hover {
+        color: rgb(255, 255, 255);
+        text-shadow: 2px 2px 5px white;
+        background: radial-gradient(closest-side, #ffffff25, #ffffff18, #00000000);
+    }
+
+
+</style>
+
+<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet">
+
+<div class="title-area">
+    <h1 class="title">Website Title</h1>
+</div>
